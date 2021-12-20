5ba1a6a643b39554d9359ded096ed629a6135c32
Mike Gaudreau <gaudreaum01@gmail.com>
2021-12-20 15:04:33 -0500

    Add thirdweb implementation for connecting to rinkby chain with injected wallet

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-// Render the App component to the DOM
+// import thirdweb
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+// select which chains we are supporting ( rinkby = 4 )
+const supportedChainIds = [4];
+
+// include type of wallet to support 
+// metamask aka 'injected-wallet'
+const connectors = {
+  injected: {},
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+    connectors={connectors}
+    supportedChainIds={supportedChainIds}>
+    <div className="landing">
+      <App />
+    </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
