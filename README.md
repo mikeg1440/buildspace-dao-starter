e5fe159fbeaabcb2a2f0af5937e4f7630dcde88b
Mike Gaudreau <gaudreaum01@gmail.com>
2021-12-26 17:59:25 -0500

    Add members page content

--- README.md
+++ README.md
@@ -13,11 +13,12 @@
   [ ] Complete script 11 - revoke roles
 [ ] Add membership page to app
  
-
+App Module: 0xb0b36EE4110d24ed571f96524437fFEbaD61f708
 ERC-1155 Membership Contract: 0x2548bA8968957daA079cC8bAE50729fcBb68A7f4
 ERC-20 Token Contract: 0xf1282343F3d5CeDdEc6B7915893ceD45BC5bBc77
 
-
 Minted Tokens: 0xb0b36EE4110d24ed571f96524437fFEbaD61f708
+
+Vote Module: 0xeA874781fAE581728473fe6ed8dE2208E2fd0397
 
 [buildspace Dashboard](https://app.buildspace.so/projects/COb520aae3-7925-42f4-a5e7-eaf718933766)
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@
 /* eslint-disable react/function-component-definition */
 import { useWeb3 } from '@3rdweb/hooks';
 import { ThirdwebSDK } from '@3rdweb/sdk';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import { ethers } from 'ethers';
 
 import SnorlaxImage from './images/snorlax.png';
 import PikachuImage from './images/pikachu-small.png';
@@ -14,13 +15,10 @@ import SquirtleImage from './images/squirtle-small.png';
 const sdk = new ThirdwebSDK('rinkeby');
 
 const bundleDropModule = sdk.getBundleDropModule('0x2548bA8968957daA079cC8bAE50729fcBb68A7f4');
+const tokenModule = sdk.getTokenModule('0xf1282343F3d5CeDdEc6B7915893ceD45BC5bBc77');
 
 export default function App() {
-  const {
-    connectWallet,
-    address,
-    provider,
-  } = useWeb3();
+  const { connectWallet, address, provider } = useWeb3();
 
   console.info(`👽 Address: ${address}`);
 
@@ -30,6 +28,61 @@ export default function App() {
   const [isClaiming, setIsClaiming] = useState(false);
 
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  // Holds the amount of token each member has in state.
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+  // The array holding all of our members addresses.
+  const [memberAddresses, setMemberAddresses] = useState([]);
+
+  // A fancy function to shorten someones wallet address, no need to show the whole thing.
+  const shortenAddress = (str) => `${str.substring(0, 6)}...${str.substring(str.length - 4)}`;
+
+  // This useEffect grabs all our the addresses of our members holding our NFT.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+    // with tokenId 0.
+    bundleDropModule
+      .getAllClaimerAddresses('0')
+      .then((addresses) => {
+        console.log('🚀 Members addresses', addresses);
+        setMemberAddresses(addresses);
+      })
+      .catch((err) => {
+        console.error('failed to get member list', err);
+      });
+  }, [hasClaimedNFT]);
+
+  // This useEffect grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Grab all the balances.
+    tokenModule
+      .getAllHolderBalances()
+      .then((amounts) => {
+        console.log('👜 Amounts', amounts);
+        setMemberTokenAmounts(amounts);
+      })
+      .catch((err) => {
+        console.error('failed to get token amounts', err);
+      });
+  }, [hasClaimedNFT]);
+
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => memberAddresses.map((memberAddress) => ({
+    memberAddress,
+    tokenAmount: ethers.utils.formatUnits(
+      // If the address isn't in memberTokenAmounts, it means they don't
+      // hold any of our token.
+      memberTokenAmounts[memberAddress] || 0,
+      18,
+    ),
+  })), [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     if (address !== null) return;
@@ -72,19 +125,29 @@ export default function App() {
         setIsClaiming(false);
         setHasClaimedNFT(true);
         // Show user new NFT
-        console.log(`🥳 Successfully minted!\n Check it out on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address}/0`);
+        console.log(
+          `🥳 Successfully minted!\n Check it out on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address}/0`,
+        );
       });
   };
 
   if (!address) {
     return (
       <div>
-        <img src={SnorlaxImage} alt="Snorlax Icon" style={{ width: '60%', margin: 'auto' }} />
+        <img
+          src={SnorlaxImage}
+          alt="Snorlax Icon"
+          style={{ width: '60%', margin: 'auto' }}
+        />
         <h1>
           Welcome to
           <span className="color-header poketext-logo">PokéDAO</span>
         </h1>
-        <button type="button" onClick={() => connectWallet('injected')} className="btn-hero">
+        <button
+          type="button"
+          onClick={() => connectWallet('injected')}
+          className="btn-hero"
+        >
           Connect to wallet
         </button>
       </div>
@@ -94,8 +157,33 @@ export default function App() {
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1 className="poketext">DAO Member Page</h1>
+        <h1 className="poketext">
+          <img src={PikachuImage} alt="Pikachu from Pokemon jumping" width="100px" />
+          DAO Member Page
+          <img src={SquirtleImage} alt="Squirtle form Pokemon dancing" width="100px" />
+        </h1>
         <p>Congratulations on being a member!</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => (
+                  <tr key={member.memberAddress}>
+                    <td>{shortenAddress(member.memberAddress)}</td>
+                    <td>{member.tokenAmount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
@@ -106,12 +194,8 @@ export default function App() {
         <img src={SquirtleImage} alt="Squirtle form Pokemon dancing" />
       </div>
       <h1>Mint your free PokéDAO Membership NFT!</h1>
-      <button
-        type="button"
-        disabled={isClaiming}
-        onClick={() => mintNft()}
-      >
-        { isClaiming ? 'Minting...' : 'Mint your NFT (Free)'}
+      <button type="button" disabled={isClaiming} onClick={() => mintNft()}>
+        {isClaiming ? 'Minting...' : 'Mint your NFT (Free)'}
       </button>
     </div>
   );
